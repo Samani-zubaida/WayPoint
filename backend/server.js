9425025536ec85cfbe57bef736a6f1053b82e9b7
Samani yusrah <samaniyusra44@gmail.com>
2026-07-12 21:52:47 +0530

    deployment change

--- backend/server.js
+++ backend/server.js
@@ -29,11 +29,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
+  cors()
 );
 
 // Request logger (optional)
