d4bfc46cc55ef4fce6c8b2a8d9c09536c4d112c8
Samani yusrah <samaniyusra44@gmail.com>
2026-07-26 21:12:03 +0530

    last

--- backend/server.js
+++ backend/server.js
@@ -36,20 +36,7 @@ const allowedOrigins = [
 ];
 
 app.use(
-  cors({
-    origin(origin, callback) {
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
+  cors()
 );
 
 
